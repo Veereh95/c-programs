681a1c38f4b7fad41e94c45319b5abb90e2a960a
Veeresh95 <veereshmk111@gmail.com>
2025-06-13 23:49:14 +0530

    c question

---
+++ c_question_08.c
@@ -0,0 +1,13 @@
+// program to solve given equation (3*x)/(y-z+k), where x=2,y=3,z=3,k=1
+#include <stdio.h>
+int main()
+{
+    int x=2,
+    y=3,
+    z=3,
+    k=1;
+    float ans=((3*x)/(y-z+k));
+    printf("\n the answer is %f",ans);
+    
+        return 0;
+}
